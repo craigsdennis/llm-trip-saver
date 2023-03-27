62637f81022baf1a97017d48f14766971db9aed9
Craig Dennis <cdennis@twilio.com>
2023-03-27 16:41:00 -0700

    Adds profile browser and talks

---
+++ profile-browser.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+import requests
+import streamlit as st
+
+from utils import CompanyTrip
+
+COMPANIES = ("shadazzle", "fitlyfe", "socimind")
+
+
+company = st.selectbox(
+    "Choose an existing Company Trip",
+    options=COMPANIES,
+    format_func=lambda x: x.title(),
+    key="company",
+)
+
+profiles_url = f"https://llm-companies.cyclic.app/api/{company}/profiles"
+
+browse_id = st.slider(f"Browse profiles for {company.title()}", max_value=50)
+
+if browse_id > 0:
+    response = requests.get(f"{profiles_url}/{browse_id}")
+    profile = response.json()
+    st.code(json.dumps(profile, indent=4))
+
+
+with st.form("company-prompt"):
+    company_prompt = st.text_area(f"This prompt is in context for {company}")
+    submitted = st.form_submit_button("Ask")
+    if (submitted):
+        company_trip = CompanyTrip.from_name(company)
+        answer = company_trip.ask(company_prompt)
+        st.markdown(answer)
+
---
+++ talk.md
@@ -0,0 +1,18 @@
+# Sure the machines are learning...but what about us humans?! 
+
+- [ ] 👋 Welcome! We're all new here! 👨‍💻👩‍💻 
+- [ ] 🤗🧠 Let's apply some Emotional Intelligence to Artificial Intelligence
+- [ ] LLMs - Large Language Models
+  - [ ] 🧱 Generative
+  - [ ] 👨‍🏫 Pre-training (45 TB)
+  - [ ] ♺ Transformer
+- [ ] 💡 Good example use cases
+- [ ] 🤷‍♂️ And some others...hallucinating?
+- [ ] 👷‍♂️ Prompt engineering
+- [ ] 📈 Speaking of context...
+- [ ] Personalization is expected
+  - [ ] 49% repeat visitors / 62% say they will leave
+- [ ] 🥳 First Party Data Party 🎊
+- [ ] 🤔 How do we let y'all play with example first party data?
+- [ ] Demo
+- [ ] 🎉 Congratulations
