86b92b412dc36742e7ec3e88fe4c547bf401b4b6
Craig Dennis <cdennis@twilio.com>
2023-08-17 11:27:21 -0700

    Updates server and gives starter index

--- server.py
+++ server.py
@@ -6,12 +6,23 @@ app = Flask(__name__)
 
 company_trip = CompanyTrip.from_name("socimind")
 
-@app.route("/company_chat")
+@app.route("/")
+def index():
+    return """
+    <form action="/company_chat">
+        <input type="text" placeholder="Enter your question" name="prompt" />
+        <button type="submit">Query CompanyTrip</button>
+    </form>
+"""
+
+
+@app.route("/company_chat", methods=["GET", "POST"])
 def company_chat():
-    prompt = request.args["prompt"]
+    prompt = request.args.get("prompt", "You forgot to include a prompt!")
     response = company_trip.ask(prompt)
     return jsonify(
         response=response,
         prompt=prompt,
+        num_messages=len(company_trip.messages)
     )
 
